9fbcb539672e368735d7f127427dd1cad4f619e2
vaibhavnalawade7 <vaibhavdnalawade396@gmail.com>
2022-05-11 06:26:24 +0530

    forHerL***

---
+++ js/js.js
@@ -0,0 +1,336 @@
+// helper functions
+const PI2 = Math.PI * 2
+const random = (min, max) => Math.random() * (max - min + 1) + min | 0
+const timestamp = _ => new Date().getTime()
+
+// container
+class Birthday {
+  constructor() {
+    this.resize()
+
+    // create a lovely place to store the firework
+    this.fireworks = []
+    this.counter = 0
+
+  }
+  
+  resize() {
+    this.width = canvas.width = window.innerWidth
+    let center = this.width / 2 | 0
+    this.spawnA = center - center / 4 | 0
+    this.spawnB = center + center / 4 | 0
+    
+    this.height = canvas.height = window.innerHeight
+    this.spawnC = this.height * .1
+    this.spawnD = this.height * .5
+    
+  }
+  
+  onClick(evt) {
+     let x = evt.clientX || evt.touches && evt.touches[0].pageX
+     let y = evt.clientY || evt.touches && evt.touches[0].pageY
+     
+     let count = random(3,5)
+     for(let i = 0; i < count; i++) this.fireworks.push(new Firework(
+        random(this.spawnA, this.spawnB),
+        this.height,
+        x,
+        y,
+        random(0, 260),
+        random(30, 110)))
+          
+     this.counter = -1
+     
+  }
+  
+  update(delta) {
+    ctx.globalCompositeOperation = 'hard-light'
+    ctx.fillStyle = `rgba(20,20,20,${ 7 * delta })`
+    ctx.fillRect(0, 0, this.width, this.height)
+
+    ctx.globalCompositeOperation = 'lighter'
+    for (let firework of this.fireworks) firework.update(delta)
+
+    // if enough time passed... create new new firework
+    this.counter += delta * 3 // each second
+    if (this.counter >= 1) {
+      this.fireworks.push(new Firework(
+        random(this.spawnA, this.spawnB),
+        this.height,
+        random(0, this.width),
+        random(this.spawnC, this.spawnD),
+        random(0, 360),
+        random(30, 110)))
+      this.counter = 0
+    }
+
+    // remove the dead fireworks
+    if (this.fireworks.length > 1000) this.fireworks = this.fireworks.filter(firework => !firework.dead)
+
+  }
+}
+
+class Firework {
+  constructor(x, y, targetX, targetY, shade, offsprings) {
+    this.dead = false
+    this.offsprings = offsprings
+
+    this.x = x
+    this.y = y
+    this.targetX = targetX
+    this.targetY = targetY
+
+    this.shade = shade
+    this.history = []
+  }
+  update(delta) {
+    if (this.dead) return
+
+    let xDiff = this.targetX - this.x
+    let yDiff = this.targetY - this.y
+    if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) { // is still moving
+      this.x += xDiff * 2 * delta
+      this.y += yDiff * 2 * delta
+
+      this.history.push({
+        x: this.x,
+        y: this.y
+      })
+
+      if (this.history.length > 20) this.history.shift()
+
+    } else {
+      if (this.offsprings && !this.madeChilds) {
+        
+        let babies = this.offsprings / 2
+        for (let i = 0; i < babies; i++) {
+          let targetX = this.x + this.offsprings * Math.cos(PI2 * i / babies) | 0
+          let targetY = this.y + this.offsprings * Math.sin(PI2 * i / babies) | 0
+
+          birthday.fireworks.push(new Firework(this.x, this.y, targetX, targetY, this.shade, 0))
+
+        }
+
+      }
+      this.madeChilds = true
+      this.history.shift()
+    }
+    
+    if (this.history.length === 0) this.dead = true
+    else if (this.offsprings) { 
+        for (let i = 0; this.history.length > i; i++) {
+          let point = this.history[i]
+          ctx.beginPath()
+          ctx.fillStyle = 'hsl(' + this.shade + ',100%,' + i + '%)'
+          ctx.arc(point.x, point.y, 1, 0, PI2, false)
+          ctx.fill()
+        } 
+      } else {
+      ctx.beginPath()
+      ctx.fillStyle = 'hsl(' + this.shade + ',100%,50%)'
+      ctx.arc(this.x, this.y, 1, 0, PI2, false)
+      ctx.fill()
+    }
+
+  }
+}
+
+let canvas = document.getElementById('birthday')
+let ctx = canvas.getContext('2d')
+
+let then = timestamp()
+
+let birthday = new Birthday
+window.onresize = () => birthday.resize()
+document.onclick = evt => birthday.onClick(evt)
+document.ontouchstart = evt => birthday.onClick(evt)
+
+  ;(function loop(){
+  	requestAnimationFrame(loop)
+
+  	let now = timestamp()
+  	let delta = now - then
+
+    then = now
+    birthday.update(delta / 1000)
+  	
+
+  })()
+
+  'use strict';
+
+/**
+ * Not doing any polyfills, this is a one-off, added fun little extra
+ * Assumes window.requestAnimationFrame support, unprefixed CSS Transforms,
+ * emoji support, and ES6 support
+ */
+
+//Each update cycle should remove this much life from a snowflake
+const LIFE_PER_TICK = 1000 / 60;
+ //Number of snowflakes
+const MAX_FLAKES = Math.min(75, screen.width / 1280 * 75);
+//The array of snow particles to be animated. They are HTMLElements
+const flakes = [];
+
+//A variety of periodic movement functions for the x-axis to create a range of snow falling models
+//The initial multiplier determines how far it moves in vw units at most, from the original
+//x-axis position
+const period = [
+    n => 5 * (Math.sin(n)),
+    n => 8 * (Math.cos(n)),
+    n => 5 * (Math.sin(n) * Math.cos(2 * n)),
+    n => 2 * (Math.sin(0.25 * n) - Math.cos(0.75 * n) + 1),
+    n => 5 * (Math.sin(0.75 * n) + Math.cos(0.25 * n) - 1)
+];
+
+//Emojis to substitute for snowflakes, just for fun 💕🎉💞😍🤗☺️
+
+const fun = ['💕', '🎁', '🎉', '💞', '🤗'];
+
+//The CSS styles for the snowflakes and container
+const cssString = `.snowfall-container {
+    display: block;
+    height: 100vh;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    -webkit-perspective-origin: top center;
+            perspective-origin: top center;
+    -webkit-perspective: 150px;
+            perspective: 150px;
+    pointer-events: none;
+    position: fixed;
+    top: 0;
+    -webkit-transform-style: preserve-3d;
+            transform-style: preserve-3d;
+    width: 100%;
+    z-index: 99999; }
+
+  .snowflake {
+    pointer-events: none;
+    color: #ddf;
+    display: block;
+    font-size: 24px;
+    left: -12px;
+    line-height: 24px;
+    position: absolute;
+    top: -12px;
+    -webkit-transform-origin: center;
+            transform-origin: center; }`;
+
+// Add a DOMContentLoaded listener, or fire the function immediately if that already happened
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+        fn();
+    }
+    else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+// Reset a flake to newly randomized values
+function resetFlake(flake) {
+    // X-axis is in vw CSS units
+    let x = flake.dataset.origX = (Math.random() * 100);
+    //Y-axis is in CSS vh units
+    let y = flake.dataset.origY = 0;
+
+    //Once in awhile, have closer snowflakes
+    //Z-axis is in CSS px units
+    let z = flake.dataset.origZ = (Math.random() < 0.1) ? (Math.ceil(Math.random() * 100) + 25) : 0;
+
+    let life = flake.dataset.life = (Math.ceil(Math.random() * 4000) + 6000); //Milliseconds
+    flake.dataset.origLife = life; //Timestamps for flake creation
+
+    flake.style.transform = `translate3d(${x}vw, ${y}vh, ${z}px)`;
+    flake.style.opacity = 1.0;
+
+    //This is the index into the period function array
+    flake.dataset.periodFunction = Math.floor(Math.random() * period.length);
+
+    if (Math.random() < 0.1) {
+        //Very small chance of some fun happening
+        flake.innerText = fun[Math.floor(Math.random() * fun.length)];
+    }
+}
+
+// Move all the snowflakes
+function updatePositions() {
+
+    flakes.forEach((flake) => {
+        // Normalize amount of time a snowfalke has been alive to the range [0, 1.0]
+        let origLife = parseFloat(flake.dataset.origLife)
+        let curLife = parseFloat(flake.dataset.life);
+        let dt = (origLife - curLife) / origLife;
+
+        if (dt <= 1.0) {
+            // Fetch this flake's personalized periodicity for x-axis movement fromt he array
+            let p = period[parseInt(flake.dataset.periodFunction)];
+            // Calculate new x-position, relative to original starting x
+            let x = p(dt * 2 * Math.PI) + parseFloat(flake.dataset.origX);
+            //Snowflakes fall to the bottom of the screen using a straight linear progression over their lifespan
+            let y = 100 * dt;
+            // Z-depth does not vary over time, although I guess it could?
+            let z = parseFloat(flake.dataset.origZ);
+            // Each update, change the CSS transformation
+            flake.style.transform = `translate3d(${x}vw, ${y}vh, ${z}px)`;
+
+            if (dt >= 0.5) {
+                //Start fading out flakes 1/2 down screen
+                flake.style.opacity = (1.0 - ((dt - 0.5) * 2));
+            }
+
+            curLife -= LIFE_PER_TICK;
+            flake.dataset.life = curLife;
+        }
+        else {
+            //Once the lifespan is exceeded, reset the flake
+            resetFlake(flake);
+        }
+    });
+
+    //Using requestAnimationFrame to update the positions for a (hopefully) smooth animation
+    window.requestAnimationFrame(updatePositions);
+}
+
+
+function appendSnow() {
+    //Append the CSS styles to the document head
+    let styles = document.createElement('style');
+    styles.innerText = cssString;
+    document.querySelector('head').appendChild(styles);
+
+    //Create the container for the snowflakes and add it to the document body
+    let field = document.createElement('div');
+    field.classList.add('snowfall-container');
+
+    //set aria-hidden and role=presentation so that screen readers don't read the emoji
+    field.setAttribute('aria-hidden', 'true');
+    field.setAttribute('role', 'presentation');
+    document.body.appendChild(field);
+
+    let i = 0;
+
+    //Using an inner function and setTimeout to delay the initial snowfall
+    //This makes it much less clumpy
+    const addFlake = () => {
+        let flake = document.createElement('span');
+        flake.classList.add('snowflake');
+        flake.setAttribute('aria-hidden', 'true');
+        flake.setAttribute('role', 'presentation');
+        flake.innerText = '🎂';
+        resetFlake(flake);
+        flakes.push(flake);
+        field.appendChild(flake);
+
+        //Recursive (delayed by timeout) call to add a flake until max reached
+        if (i++ <= MAX_FLAKES) {
+            setTimeout(addFlake, Math.ceil(Math.random() * 300) + 100);
+        }
+    };
+    addFlake();
+
+    updatePositions();
+}
+
+ready(appendSnow);
